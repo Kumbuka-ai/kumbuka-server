025dfd84ff16f15813b4d0b3d39c183ab5235fe7
jbaconsult <github@johannesbayer.de>
2026-06-09 10:11:25 +0200

    fix(test): add ArgumentMatchers.eq import

--- backend/server/src/test/java/ai/kumbuka/admin/AdminUsersResourceTest.java
+++ backend/server/src/test/java/ai/kumbuka/admin/AdminUsersResourceTest.java
@@ -18,6 +18,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
